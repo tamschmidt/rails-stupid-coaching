a60dd33e20ab23f205e4555740afc0c26079f4cf
Tamara Schmidt <tamarafrrr@gmail.com>
2019-02-11 15:41:37 -0200

    exercise 1-Stupid Coaching done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'ask', to: 'pages#ask', as: :ask
+  get 'answer', to: 'pages#answer', as: :answer
+
+  root to: 'pages#ask'
 end
